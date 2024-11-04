ac2ebbea3a838ddd1f06e228252bd69ded81bdf7
youngchive <102659915+youngchive@users.noreply.github.com>
2024-11-04 17:58:53 +0900

    Feat: 초기 환경설정

---
+++ src/main/java/com/elice/artBoard/ArtBoardApplication.java
@@ -0,0 +1,13 @@
+package com.elice.artBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/artBoard/ArtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.artBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
